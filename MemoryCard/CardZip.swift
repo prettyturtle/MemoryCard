b6d6692b1c0f2df9a626a185f864ff08f8ca9287
prettyturtle <unifconti@gmail.com>
2023-04-25 22:20:22 +0900

    MockData

--- MemoryCard/CardZip.swift
+++ MemoryCard/CardZip.swift
@@ -13,4 +13,11 @@ struct CardZip: Codable {
     let folderName: String              // 카드 폴더 명
     let cards: [Card]                   // 카드 배열
     let mIdx: String                    // 유저 아이디
+    
+    
+    static var mockData: [CardZip] {
+        return (1...10).map {
+            CardZip(folderName: "TEST\($0)", cards: [Card(id: $0, front: Card.CardContent(content: "front \($0)"), back: Card.CardContent(content: "back \($0)"))], mIdx: AuthManager.shared.getCurrentUser()?.uid ?? "")
+        }
+    }
 }
