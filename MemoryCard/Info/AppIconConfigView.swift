c4b21dfcebb2c4bca07f5eccb236d113a4f598d8
Cobugi <unifconti@gmail.com>
2024-11-18 00:41:24 +0900

    1.6

--- MemoryCard/Info/AppIconConfigView.swift
+++ MemoryCard/Info/AppIconConfigView.swift
@@ -8,84 +8,84 @@
 import SwiftUI
 
 struct AppIconConfigView: View {
-    
-    private let columns = Array(repeating: GridItem(.flexible()), count: 2)
-    
-    private let iconNames = [
-        "AppIcon_Black",
-        "AppIcon_Pink",
-        "AppIcon_Purple",
-        "AppIcon_Blue",
-        "AppIcon_Yellow"
-    ]
-    
-    @State var currentAppIcon: String?
-    
-    var body: some View {
-        GeometryReader { proxy in
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(iconNames, id: \.self) { iconName in
-                        if let image = UIImage(named: iconName) {
-                            let imageSize = (proxy.size.width - 48.0) / 2.0
-                            
-                            Image(uiImage: image)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: imageSize, height: imageSize)
-                                .overlay {
-                                    if let currentAppIcon = currentAppIcon,
-                                       currentAppIcon == iconName {
-                                        RoundedRectangle(cornerRadius: 12.0)
-                                            .stroke(.orange, lineWidth: 2.0)
-                                    } else {
-                                        RoundedRectangle(cornerRadius: 12.0)
-                                            .stroke(.gray.opacity(0.3), lineWidth: 1.0)
-                                    }
-                                }
-                                .onTapGesture {
-                                    currentAppIcon = iconName
-                                    
-                                    let selectedIconName = iconName == "AppIcon_Black" ? nil : iconName
-                                    
-                                    UIApplication
-                                        .shared
-                                        .setAlternateIconName(selectedIconName) { error in
-                                            if let _ = error {
-                                                return
-                                            }
-                                            
-                                            UserDefaults.standard.set(selectedIconName, forKey: APP_ICON)
-                                            
-                                            moveToBackground()
-                                        }
-                                }
-                        }
-                    }
-                }
-            }
-        }
-        .navigationTitle("앱 아이콘 변경")
-        .navigationBarTitleDisplayMode(.inline)
-        
-        .onAppear {
-            let currAppIcon = UserDefaults.standard.string(forKey: APP_ICON)
-            
-            currentAppIcon = currAppIcon ?? iconNames.first
-        }
-    }
-    
-    // 백그라운드로 이동
-    private func moveToBackground() {
-        let selector = NSSelectorFromString("suspend")
-        let sharedUIApplication = UIApplication.perform(NSSelectorFromString("sharedApplication"))?.takeUnretainedValue()
-        _ = sharedUIApplication?.perform(selector, with: nil)
-    }
+	
+	private let columns = Array(repeating: GridItem(.flexible()), count: 2)
+	
+	private let iconNames = [
+		"AppIcon_Black",
+		"AppIcon_Pink",
+		"AppIcon_Purple",
+		"AppIcon_Blue",
+		"AppIcon_Yellow"
+	]
+	
+	@State var currentAppIcon: String?
+	
+	var body: some View {
+		GeometryReader { proxy in
+			ScrollView {
+				LazyVGrid(columns: columns) {
+					ForEach(iconNames, id: \.self) { iconName in
+						if let image = UIImage(named: iconName) {
+							let imageSize = (proxy.size.width - 48.0) / 2.0
+							
+							Image(uiImage: image)
+								.resizable()
+								.aspectRatio(contentMode: .fit)
+								.frame(width: imageSize, height: imageSize)
+								.overlay {
+									if let currentAppIcon = currentAppIcon,
+									   currentAppIcon == iconName {
+										RoundedRectangle(cornerRadius: 12.0)
+											.stroke(.orange, lineWidth: 2.0)
+									} else {
+										RoundedRectangle(cornerRadius: 12.0)
+											.stroke(.gray.opacity(0.3), lineWidth: 1.0)
+									}
+								}
+								.onTapGesture {
+									currentAppIcon = iconName
+									
+									let selectedIconName = iconName == "AppIcon_Black" ? nil : iconName
+									
+									UIApplication
+										.shared
+										.setAlternateIconName(selectedIconName) { error in
+											if let _ = error {
+												return
+											}
+											
+											UserDefaults.standard.set(selectedIconName, forKey: APP_ICON)
+											
+											moveToBackground()
+										}
+								}
+						}
+					}
+				}
+			}
+		}
+		.navigationTitle("앱 아이콘 변경")
+		.navigationBarTitleDisplayMode(.inline)
+		
+		.onAppear {
+			let currAppIcon = UserDefaults.standard.string(forKey: APP_ICON)
+			
+			currentAppIcon = currAppIcon ?? iconNames.first
+		}
+	}
+	
+	// 백그라운드로 이동
+	private func moveToBackground() {
+		let selector = NSSelectorFromString("suspend")
+		let sharedUIApplication = UIApplication.perform(NSSelectorFromString("sharedApplication"))?.takeUnretainedValue()
+		_ = sharedUIApplication?.perform(selector, with: nil)
+	}
 }
 
 
 struct AppIconConfigView_Previews: PreviewProvider {
-    static var previews: some View {
-        AppIconConfigView()
-    }
+	static var previews: some View {
+		AppIconConfigView()
+	}
 }
